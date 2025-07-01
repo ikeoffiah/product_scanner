f29849f7de417def5011e8b65268155a838961e2
IKEOFFIAH PIUS <31507347+ikeoffiah@users.noreply.github.com>
2025-07-01 09:43:23 +0000

    added scanscrren page to main

--- product_scanner/lib/screens/custom_widget/corner_stone_indicator.dart
+++ product_scanner/lib/screens/custom_widget/corner_stone_indicator.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:product_scanner/screens/helper/colors.dart';
 
 class CornerStoneIndicator extends StatefulWidget {
   const CornerStoneIndicator({required this.index, super.key});
@@ -10,8 +11,25 @@ class CornerStoneIndicator extends StatefulWidget {
 }
 
 class _CornerStoneIndicatorState extends State<CornerStoneIndicator> {
+  final positions = [
+    const Alignment(-1, -1),
+    const Alignment(1, -1),
+    const Alignment(-1, 1),
+    const Alignment(1, 1),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Align(
+      alignment: positions[widget.index],
+      child: Container(
+        width: 20,
+        height: 20,
+        margin: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          border: Border.all(color: kGreenAccentColor, width: 2),
+          borderRadius: BorderRadius.circular(6),
+        ),
+      ),
+    );
   }
 }
